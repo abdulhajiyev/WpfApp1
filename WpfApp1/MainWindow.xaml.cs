39cb753f62b2d7d4710b089c5e258e2af6c01387
Hacıyev Rəhman Hörmət <Haci_jt6q@itstep.edu.az>
2021-09-03 21:49:02 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+        
+            MessageBox.Show("Salam", "Caption", MessageBoxButton.YesNo, MessageBoxImage.Information);
+
+        }
+    }
+}
