32781442b0b25b2023798afd7e9fceee2651b816
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-09-04 15:00:34 +0400

    Done

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.Remoting.Channels;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace WpfApp1
@@ -8,16 +12,41 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Random Random { get; set; } = new Random();
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btn_Click(object sender, RoutedEventArgs e)
-        {
-        
-            MessageBox.Show("Salam", "Caption", MessageBoxButton.YesNo, MessageBoxImage.Information);
 
+        private SolidColorBrush CreateColor()
+        {
+
+            var r = Convert.ToByte(Random.Next(0, 255));
+            var g = Convert.ToByte(Random.Next(0, 255));
+            var b = Convert.ToByte(Random.Next(0, 255));
+
+            return new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+
+        private void Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Background = CreateColor();
+            MessageBox.Show(btn.Content.ToString());
+        }
+
+        private void Button_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button btn = sender as Button;
+            BtnGrid.Children.Remove(btn);
+            Title += btn.Content.ToString();
+        }
+
+        private void Btn_Load(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Background = CreateColor();
         }
     }
 }
